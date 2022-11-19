cd0967f052adc8cee7bdbc687d83a1e09932bff3
Sarah Mukuti <cesarwrites24@gmail.com>
2022-11-19 22:42:27 +0300

    add all other components

---
+++ src/API/api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+export const fetchCasesForAll = async () => {
+
+    const options = {
+      method: 'GET',
+      url: 'https://covid-193.p.rapidapi.com/statistics',
+      headers: {
+        'X-RapidAPI-Key': '3d29842203msh762874f38de76fep195411jsnd6053eac3e1d',
+        'X-RapidAPI-Host': 'covid-193.p.rapidapi.com'
+      }
+    };
+    
+  const { data } = axios.request(options).then(function (response) {
+    const modifiedData = data?.map((statistics) => ({
+        totalCases: statistics.cases.total,
+        totalDeaths: statistics.deaths.total,
+        country:statistics.country,
+        totalRecovered:statistics.cases.recovered,
+        totalPopulation:statistics.population,
+        date: statistics.day,
+      }));
+      return modifiedData;
+    }).catch(function (error) {
+        console.error(error);
+    });
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { Container } from "@material-ui/core";
+import React, {useState,useEffect} from "react";
 import './App.css';
+import NavBar from './components/Navbar/NavBar';
+import Chart from './components/Chart/Chart';
+import ShowCase from './components/Showcase/ShowCase';
+import Table from './components/table/DataTable';
+import Loader from './components/loader';
+import { fetchCasesForAll} from './API/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [loading, setLoading] = useState(false);
+
+  const [tableData, setTableData] = useState([]);
+
+  const [totalData, setTotalData] = useState({
+    DataTransferItemList: 0,
+    totalCases: 0,
+    totalRecovered: 0,
+    totalConfirmed: 0,
+    totalPopulation: 0,
+    totalDeaths: 0,
+    country: ''
+  });
+
+  useEffect(() => {
+    getGlobalData();
+  }, []);
+
+  const getGlobalData = () => {
+    setLoading(true);
+    setTableData([]);
+    fetchCasesForAll().then((modifiedData) => {
+      setTableData(modifiedData);
+      setTotalData({
+        modifiedData
+      });
+
+      setLoading(false);
+    });
+  };
+
+  return(
+    <>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        <Container maxWidth='xl'>
+          <NavBar
+            
+            getGlobalData={getGlobalData}
+           
+          />
+          <ShowCase totalData={totalData} />
+          
+          <Table tableData={tableData} setTotalData={setTotalData} />
+        </Container>
+      </>
+    )}
+  </>
+);
 }
 
 export default App;
